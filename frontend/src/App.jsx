c9b26ecb136245755578db29a9223893ee5a1114
zaki-ramadhan <zakiram4dhan@gmail.com>
2025-08-18 20:15:44 +0700

    docs: add tech stack badges

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,12 +2,11 @@ import { Route, Routes } from 'react-router'
 import Homepage from './pages/Homepage'
 import NoteDetailPage from './pages/NoteDetailPage'
 import CreatePage from './pages/CreatePage'
-import toast from 'react-hot-toast'
+import Navbar from './components/Navbar';
 
 const App = () => {
   return (
     <>
-    <button onClick={() => toast.success("Congrats!")}>Click me</button>
       <Routes>
         <Route path="/" element={<Homepage />}></Route>
         <Route path="/note/:id" element={<NoteDetailPage />}></Route>
--- frontend/src/pages/Homepage.jsx
+++ frontend/src/pages/Homepage.jsx
@@ -1,10 +1,31 @@
-import React from 'react'
+import { useEffect, useState } from 'react';
+import Navbar from './../components/Navbar';
+import RateLimitedUI from '../components/RateLimitedUI';
 
 const Homepage = () => {
+  const { isRateLimited, setIsRateLimited } = useState(true);
+  const { note, setNote } = useState([]);
+  const { loading, setLoading } = useState(true);
+
+  // fetch data
+  useEffect(() => {
+    const getNotes = async () => {
+      try {
+        const res = await fetch('http://localhost:5000/api/notes');
+        const fetchedData = await res.json();
+        console.log(fetchedData);
+      } catch (error) {
+        console.error("Error fetching notes.");
+      }
+    }
+  }, []);
+
   return (
-    <>
-      Homepage
-    </>
+    <div className='min-h-screen'>
+      <Navbar />
+
+      {isRateLimited && <RateLimitedUI />}
+    </div>
   )
 }
 
