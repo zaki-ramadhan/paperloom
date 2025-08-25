55d5bad793baaf659986804954d5a71178b97c8b
zaki-ramadhan <zakiram4dhan@gmail.com>
2025-08-25 17:00:09 +0700

    fix: change incorrect path in server

--- backend/src/server.js
+++ backend/src/server.js
@@ -34,7 +34,7 @@ if (process.env.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, "../frontend/dist")))
     
     // any request
-    app.get("*", (req, res) => {
+    app.get("/*id", (req, res) => {
         res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"))
     })
 }
