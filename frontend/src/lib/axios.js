32be186cbf563635fd96344e0e8aab5a61466f1b
zaki-ramadhan <zakiram4dhan@gmail.com>
2025-08-25 17:30:03 +0700

    feat: prepared for the deployment

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,8 +1,11 @@
 import axios from "axios"
 
+// inm production, there's no localhost so we have to make this dynamic
+const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5000/api" : "/api";
+
 // Create axios instance with global base URL for all API requests
 const api = axios.create({
-    baseURL : "http://localhost:5000/api"
+    baseURL : BASE_URL
 });
 
 export default api;
